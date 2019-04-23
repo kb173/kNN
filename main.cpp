e7fea19edc18d8fb3ac011a8cacea7a8aa08241e
karl <karl.bittner@outlook.com>
2019-04-23 23:06:15 +0200

    Added temporary performance test in main

--- main.cpp
+++ main.cpp
@@ -7,14 +7,52 @@
 #include "CrossValidation.h"
 #include <map>
 
+double randomCoordinate() {
+    return (0.5 - ((double) rand() / (RAND_MAX))) * 3.0;
+}
+
+int performanceTest() {
+        CSVReader reader;
+    std::vector<std::vector<double>> data = reader.readAs2dArray(
+            "/home/karl/Data/Technikum/SEM4/MLE/kNN/datasets/winequality-red.csv", ';');
+    ZScore().standardize(data);
+
+    KDTree kdtree = KDTree();
+
+    auto sortable = ZScore().twoDimVectorToPoints(data);
+    KDTreePreparation().insertSorted(sortable, kdtree);
+
+    int amount = 1500;
+    int k = 40;
+
+    clock_t searchBegin = clock();
+    for (int i = 0; i < amount; i++) {
+        ClassifiedPoint searchPoint = ClassifiedPoint(std::vector<double>{randomCoordinate(), randomCoordinate(),
+                                                                          randomCoordinate(), randomCoordinate(),
+                                                                          randomCoordinate(), randomCoordinate(),
+                                                                          randomCoordinate(), randomCoordinate(),
+                                                                          randomCoordinate(), randomCoordinate(),
+                                                                          randomCoordinate()}, 5);
+
+        std::shared_ptr search = std::make_shared<Point>(searchPoint);
+        kdtree.search(search, k);
+    }
+
+    clock_t searchEnd = clock();
+    auto elapsedTime = double(searchEnd - searchBegin) / 1000000;
+    std::cout << "Training took " << elapsedTime << " seconds!" << std::endl;
+
+    return 0;
+}
+
 int main() {
     CSVReader reader;
-    std::vector<std::vector<double>> data = reader.readAs2dArray("/home/mathias/Documents/FH/sem_4/MLE/kNN/winequality-red.csv", ';');
+    std::vector<std::vector<double>> data = reader.readAs2dArray("/home/karl/Data/Technikum/SEM4/MLE/kNN/datasets/winequality-red.csv", ';');
     ZScore().standardize(data);
     KFold validator;
     std::vector<std::vector<std::vector<double>>> blocks = validator.getFoldedDataBlocks(data, 10);
 
-    int k = 9; // k points are evaluated (as in kNN)
+    int k = 30; // k points are evaluated (as in kNN)
 
     clock_t validateBegin = clock();
 
